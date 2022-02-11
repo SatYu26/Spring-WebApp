55596ec5a88fb5ae2a689161b5bcdad0e2d1a602
SatYu26 <satyamgoyal008@gmail.com>
2022-02-12 03:11:02 +0530

    Model and view for getting and sending data and objects

--- src/main/java/com/example/MyWebApp/HomeController.java
+++ src/main/java/com/example/MyWebApp/HomeController.java
@@ -5,17 +5,19 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(HttpServletRequest req) {
+    public ModelAndView home(@RequestParam("name") String myName) {
 
-        HttpSession session = req.getSession();
-        String name = req.getParameter("name");
-        System.out.println("hi " + name);
-        session.setAttribute("name", name);
-        return "home";
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", myName);
+        mv.setViewName("home");
+
+        return mv;
     }
 }
